9fd4ce4fd49ebd4dc923558b7d2f7e9c884942e4
n0t-4m17h <kovoor71@gmail.com>
2024-07-20 01:33:53 +1000

    AUTH: login & rego UI done

--- frontend/src/app/auth/login/page.js
+++ frontend/src/app/auth/login/page.js
@@ -1,16 +1,87 @@
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
-import Button from '@mui/material/Button';
+import { Button, Alert } from '@mui/material';
+import { Box } from '@mui/system';
+
+import AuthStyles from '@/styles/auth.styles';
+// import { API_URL } from '../../config';
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alertData, setAlertData] = useState({ 
+    show: false, severity: 'error', message: '',
+  });
+
+  const handleCloseAlert = () => {
+    setAlertData({ ...alertData, show: false });
+  };
+
+  const handleLogin = () => {
+    // login input handling logic
+    if (email.length === 0 || password.length === 0) {
+      setAlertData({ 
+        show: true, severity: 'error', message: 'Missing input data', 
+      });
+      return;
+    }
+    // Registration logic (e.g., API call) goes here
+  };
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>LOGIN to get started!</h1>
-      <p>Please login to continue.</p>
-      <Link href='/auth/register' passHref>
-        <Button variant='contained' color='primary' size='medium'>
-          Register?
-        </Button>
-      </Link>
-    </div>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      textAlign: 'center',
+      marginTop: '50px',
+      width: '100%',
+      fontFamily: ['monospace'],
+    }}>
+      <h1>Login to your account!</h1>
+      <AuthStyles.authContent>
+        {
+          alertData.show && 
+          <Alert 
+            variant="filled"
+            severity={alertData.severity}
+            onClose={handleCloseAlert}
+          >
+            {alertData.message}
+          </Alert>
+        }
+        Email: <input
+          type='text'
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+          placeholder="Enter your email"
+        /> <br/>
+        Password: <input
+          type='text'
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          placeholder="Enter your password"
+        /> <br/>
+
+        <AuthStyles.authButtonDiv>
+          <Button 
+            variant='contained' 
+            color='success' 
+            size='medium' 
+            onClick={handleLogin}
+          >
+            Login
+          </Button> <br/>
+          <Link href='/auth/register' passHref>
+            <Button variant='contained' color='primary' size='medium'>
+              Register?
+            </Button>
+          </Link>
+        </AuthStyles.authButtonDiv>
+      </AuthStyles.authContent>
+      
+    </Box>
   );
 }
--- frontend/src/styles/auth.styles.js
+++ frontend/src/styles/auth.styles.js
@@ -2,10 +2,21 @@ import { styled } from '@mui/material';
 
 const authContent = styled('div')({
   // general container
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '10px',
+  fontSize: '1.25rem',
+  marginTop: '30px',
 });
 
 const authButtonDiv = styled('div')({
   // submit button container
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  width: '250px',
 });
 
 const AuthStyles = {
